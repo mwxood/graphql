8d90f157272ba98ec4e410a469e2fd94e7fe3b5c
mwxood <mwxood@gmail.com>
2020-05-29 12:54:54 +0300

    added

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql');
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+const root = {
+    message: () => 'Hello GraphQL'
+};
+const app = express();
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
